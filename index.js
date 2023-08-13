6b759ea5c723d54672b039bfe9f49b554114d836
winqdev <103835411+winqdev@users.noreply.github.com>
2023-08-13 20:52:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,18 +8,21 @@
 ****************************/
 module.exports = {
     steamid(steamid) {
-        var regex = /^STEAM_\d:\d:\d+$/.test(steamid);
-        console.log(regex);
-        return
+        if(typeof steamid !== 'string') {
+            throw new Error('Not a string!');
+        };
+        return /^STEAM_\d:\d:\d+$/.test(steamid);
     },
     steamid3(steamid3) {
-        var regex = /^\[U:1:[0-9]+\]$/.test(steamid3);
-        console.log(regex);
-        return;
+        if(typeof steamid3 !== 'string') {
+            throw new Error('Not a string!');
+        };
+        return /^\[U:1:[0-9]+\]$/.test(steamid3);
     },
     steamid64(steamid64) {
-        var regex = /^(\d{17})$/.test(steamid64);
-        console.log(regex);
-        return;
+        if(typeof steamid64 !== 'string') {
+            throw new Error('Not a string!');
+        };
+        return /^(\d{17})$/.test(steamid64);
     }
 }
